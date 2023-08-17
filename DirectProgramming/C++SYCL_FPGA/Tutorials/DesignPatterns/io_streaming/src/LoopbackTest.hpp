807d99597c2c7e09e5ff8e166f29b5d38b8cd0fd
pajgaonk <paritosh.ajgaonkar@intel.com>
2023-08-17 16:18:53 -0700

    updated code to fix accessor issue

--- DirectProgramming/C++SYCL_FPGA/Tutorials/DesignPatterns/io_streaming/src/LoopbackTest.hpp
+++ DirectProgramming/C++SYCL_FPGA/Tutorials/DesignPatterns/io_streaming/src/LoopbackTest.hpp
@@ -59,10 +59,10 @@ event SubmitLoopbackKernel(queue& q, size_t count, bool& passed) {
   buffer<unsigned long int, 1> buf_out(dataout_host, range<1>(PIPE_COUNT * OUTER_LOOP_COUNT * INNER_LOOP_COUNT ));
 
   event kevent = q.submit([&] (handler& h) {
-    auto in = buf_in.get_host_access(h);
+    auto in = buf_in.get_access(h);
     //auto in = buf_in.get_access<access::mode::read_write>(h);
 
-    auto out = buf_out.get_host_access(h);
+    auto out = buf_out.get_access(h);
     //auto out = buf_out.get_access<access::mode::read_write>(h);
 
     h.single_task<LoopBackMainKernel>([=] {
@@ -83,7 +83,7 @@ event SubmitLoopbackKernel(queue& q, size_t count, bool& passed) {
   });
   });
 
-  buf_out.get_host_access();
+  buf_out.get_access();
   //buf_out.get_access<access::mode::read>();
 
   for (size_t i = 0; i < (PIPE_COUNT*OUTER_LOOP_COUNT*INNER_LOOP_COUNT); i++) {
